977464c0d0eafa5c8a78f3ff1af4fe50cf43ce27
Hyochang-Son@main notebook <dagdf20@naver.com>
2024-11-04 14:26:03 +0900

    시작

---
+++ Lecture13_Game_World/game_world.py
@@ -0,0 +1,29 @@
+#world = [] # 단일 계층 구조
+
+# world [0]에는 백그라운드 객체들.. 즉 맨 아래에 그려야 할 객체들
+# world [1]에는 포어그라운드 객체들... 위에 그려야 할 객체들
+world = [[],[]]
+
+def add_object(o, depth):
+    world[depth].append(o)
+
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
+
+
+
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return # 지우는 미션은 달성. 다른 요소는 더 이상 체크 할 필요 없음.
+    print('Error: 존재하지 않는 객체를 지운다고??')
